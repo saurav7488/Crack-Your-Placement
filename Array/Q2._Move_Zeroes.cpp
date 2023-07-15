edc69a0b5cf195fbee254fad25d2e2387ce5ab40
SAURAV <jitenderkumarmukul@gmail.com>
2023-07-15 23:55:48 +0530

    Array question

---
+++ Array/Q2._Move_Zeroes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int i=0,j=0;
+        while(i<n and j<n) {
+            if(nums[j]==0) j++;
+            else{
+               swap(nums[i],nums[j]);
+               i++,j++;
+            }
+        }
+    }
+};
