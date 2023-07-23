f229dd6fceb2b3088999f9512b9bb85423865598
SAURAV <jitenderkumarmukul@gmail.com>
2023-07-23 16:39:58 +0530

    Q3 solve

---
+++ Tree/Q3_Subtree_of_Another_Tree.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+   public:
+    bool same(TreeNode* root, TreeNode* subRoot) {
+          if(root==NULL and subRoot==NULL) return true;
+         if(root==NULL or subRoot==NULL) return false;
+         if(root->val!=subRoot->val) return false;
+         return same(root->left,subRoot->left) and same(root->right,subRoot->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+         if(root==NULL) return false;
+          if(same(root,subRoot)) return true;
+         return isSubtree(root->left,subRoot) or isSubtree(root->right,subRoot);
+    }
+};
