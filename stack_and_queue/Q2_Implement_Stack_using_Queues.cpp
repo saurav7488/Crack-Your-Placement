eda7a1ed3a1971befe47f064128df95d3f6a4e09
SAURAV <jitenderkumarmukul@gmail.com>
2023-07-24 13:07:04 +0530

    Q2 solve

---
+++ stack_and_queue/Q2_Implement_Stack_using_Queues.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MyStack {
+public:
+   queue<int>q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i=0;i<q.size()-1;i++){
+             int m=q.front();
+            q.pop();
+            q.push(m);
+        }
+    }
+    
+    int pop() {
+       int res=q.front();
+       q.pop();
+       return res;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.size()==0;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
