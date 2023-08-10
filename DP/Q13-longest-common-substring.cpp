24f42a6b7d511b606734639e78b77a3c931ce49d
SAURAV <jitenderkumarmukul@gmail.com>
2023-08-10 22:46:17 +0530

    Q13 solve

---
+++ DP/Q13-longest-common-substring.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    // TC->O(nxm)
+    // sc->O(nxm)
+    int longestCommonSubstr (string s1, string s2, int n, int m)
+    {
+        // your code here
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        int ans=0;
+        for(int i=1;i<=n;i++) {
+             for(int j=1;j<=m;j++) {
+                  if(s1[i-1]==s2[j-1]) {
+                       dp[i][j]=1+dp[i-1][j-1];
+                  }
+                  ans=max(ans,dp[i][j]);
+             }
+        }
+        return ans;
+    }
+};
+
