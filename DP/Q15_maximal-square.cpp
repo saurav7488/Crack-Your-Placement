5701de39252d44f429a4590c1d480fb0ea35dc66
SAURAV <jitenderkumarmukul@gmail.com>
2023-08-11 20:29:57 +0530

    Q15 solve

---
+++ DP/Q15_maximal-square.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+// / TC->O(NxM)
+  // SC->O(NxM)
+    int maximalSquare(vector<vector<char>>& matrix) {
+         int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<n;i++) if(matrix[i][0]=='1') dp[i][0]=1;
+        for(int i=0;i<m;i++) if(matrix[0][i]=='1')  dp[0][i]=1;
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<m;j++) {
+                if(matrix[i][j]=='1')
+                dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+               int res=dp[i][j]*dp[i][j];
+                 ans=max(ans,res);
+            }
+            // cout<<"\n";
+        }
+        return ans;
+    }
+};
