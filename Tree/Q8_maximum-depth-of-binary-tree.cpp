8dbb4c8a968961433904f05a7ebf20cc7cac0034
SAURAV <jitenderkumarmukul@gmail.com>
2023-08-15 23:49:31 +0530

    Q8 solve

---
+++ Tree/Q8_maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//  Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+    int fun(TreeNode*root) {
+         if(root==NULL) return 0;
+         int l=fun(root->left);
+         int r=fun(root->right);
+         return 1+max(l,r);
+    }
+    int maxDepth(TreeNode* root) {
+       return fun(root);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
