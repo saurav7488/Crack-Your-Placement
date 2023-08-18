0cc1ab1479749f2f682c1ac52470fe3c80f6aa53
SAURAV <jitenderkumarmukul@gmail.com>
2023-08-19 00:49:43 +0530

    Q15 solve

---
+++ Tree/Q15_balanced-binary-tree.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+///**
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    int fun(TreeNode*root,bool&ans) {
+          if(root==NULL) return 0;
+          int l=fun(root->left,ans);
+          int r=fun(root->right,ans);
+          if(abs(l-r)>1) ans=false;
+          return max(l,r)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+       bool ans=true;
+        fun(root,ans);
+        return ans;
+    }
+};
