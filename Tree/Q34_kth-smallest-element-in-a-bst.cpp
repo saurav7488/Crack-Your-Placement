9c4eaf8e34aa88426d875e89e21ef3f70a2152bd
SAURAV <jitenderkumarmukul@gmail.com>
2023-08-26 15:06:01 +0530

    Q34 solve

---
+++ Tree/Q34_kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+//**
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+   int ans=0;
+   void fun(TreeNode*root,int&k) {
+         if(root==NULL) return;
+         fun(root->left,k);
+         k--;
+         if(k==0) ans=root->val;
+         fun(root->right,k);
+   }
+    int kthSmallest(TreeNode* root, int k) {
+          fun(root,k);
+          return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
