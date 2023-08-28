fc69d65198bac8066a079a3f8d67718ce5f64a03
SAURAV <jitenderkumarmukul@gmail.com>
2023-08-28 15:09:28 +0530

    Q40 solve

---
+++ Tree/Q40_serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,73 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Codec {
+// public:
+
+//     // Encodes a tree to a single string.
+//     string serialize(TreeNode* root) {
+//         if(root==NULL){return "";}
+//         string str;
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         while(!q.empty())
+//         {
+//             TreeNode* curr=q.front();
+//             q.pop();
+//             if(curr==NULL)
+//             {
+//                 str.append("#,");
+//             }
+//             else
+//             {
+//                 str.append(to_string(curr->val)+',');
+//             }
+//            if(curr!=NULL)
+//            {
+//                q.push(curr->left);
+//                q.push(curr->right);
+//            }
+//         }
+//        return str; 
+//     }
+
+//     // Decodes your encoded data to tree.
+//     TreeNode* deserialize(string data) {
+//         if(data.size()==0){return NULL;}
+//         stringstream ss(data);
+//         string str;
+//         getline(ss,str,',');
+//         queue<TreeNode*> q;
+//         TreeNode* root = new TreeNode(stoi(str));
+//         q.push(root);
+//         while(!q.empty())
+//         {
+//             TreeNode* curr = q.front();
+//             q.pop();
+            
+//             getline(ss,str,',');
+//             if(str=="#")
+//             {
+//                 curr->left=NULL;
+//             }
+//             else
+//             {
+//                 TreeNode* leftNode = new TreeNode(stoi(str));
+//                 curr->left = leftNode;
+//                 q.push(leftNode);
+//             }
+            
+//             getline(ss,str,',');
+//             if(str=="#")
+//             {
+//                 curr->right = NULL;
+//             }
+//             else
+//             {
+//                 TreeNode* rightNode = new TreeNode(stoi(str));
+//                 curr->right = rightNode;
+//                 q.push(rightNode);
+//             }
+//         }
+//       return root;  
+//     }
+// };
